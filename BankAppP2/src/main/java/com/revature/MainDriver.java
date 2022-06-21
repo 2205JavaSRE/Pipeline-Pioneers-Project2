ab59afb6f871deb68e12aec58c77980cf8d799d4
ZacBurnett <zachary710@revature.net>
2022-06-21 10:04:15 -0700

    Added in configuration for high latency requests.

--- BankAppP2/src/main/java/com/revature/MainDriver.java
+++ BankAppP2/src/main/java/com/revature/MainDriver.java
@@ -13,8 +13,11 @@ public class MainDriver {
         Javalin serverInstance = Javalin.create(
                 javalinConfig -> {
                     javalinConfig.registerPlugin(new MicrometerPlugin(Monitoring.getRegistry()));
-                    javalinConfig.requestLogger((context, ms) -> {
-                        if (ms > 200) Monitoring.incrementHighLatencyCounter();
+                    javalinConfig.requestLogger((context, ms) -> { //Logging high latency requests
+                        if (ms > 200 && !context.path().equals("/metrics")
+                                && context.res.getStatus() != 404
+                                && context.res.getStatus() != 500)
+                            Monitoring.incrementHighLatencyCounter();
                     });
                 }
         ).start(7500);
